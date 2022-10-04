47fd200abbf13d8fc57036fc642d6fc9c0930cf0
Choi Sangmin <61264739+choism3938@users.noreply.github.com>
2022-10-04 13:51:59 +0900

    Create 1018_체스판 다시 칠하기.cpp

---
+++ boj/단계별로 풀어보기/브루트 포스/1018_체스판 다시 칠하기.cpp
@@ -0,0 +1,81 @@
+// boj 1018. 체스판 다시 칠하기
+// https://www.acmicpc.net/problem/1018
+// brute force Algorithm
+// W로 시작하는 8 x 8 체스판, B로 시작하는 8 x 8 체스판을 저장 후, 전체 영역에서 8x8 잘라서 비교, 가장 차이점 없는 경우를 저정한다. 
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+using namespace std;
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+string board[50];
+int WB_cnt(int x, int y)
+{
+    int cnt = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(board[x+i][y+j] != WB[i][j])
+                cnt++;
+        }
+
+    }
+    return cnt;
+}
+int BW_cnt(int x, int y)
+{
+    int cnt = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(board[x+i][y+j] != BW[i][j])
+                cnt++;
+        }
+
+    }
+    return cnt;
+}
+int main() {
+    int size[2];
+    int cnt;
+    int min_val = 12345;
+    pair<int, int> p1;
+    cin >> p1.first >> p1.second;
+    for(int i = 0; i < p1.first; i++)
+        cin >> board[i];
+    for(int i = 0; i + 8 <= p1.first; i++)
+    {
+        for(int j = 0; j + 8 <= p1.second; j++)
+        {
+            int tmp;
+            tmp = min(WB_cnt(i,j),BW_cnt(i,j));
+            if(tmp < min_val) {
+                min_val = tmp;
+            }
+        }
+    }
+    cout << min_val;
+    return 0;
+}
